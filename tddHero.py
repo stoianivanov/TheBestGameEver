1651b052766251d4bab47f55d99986cfe38fb0d0
Rositsazz <ross_zz@mail.bg>
2015-04-17 13:10:17 +0300

    add new feature in Hero

--- tddHero.py
+++ tddHero.py
@@ -1,5 +1,7 @@
 import unittest
 from hero import Hero
+from weapon import Weapon
+from spell import Spell
 
 
 class HeroTest(unittest.TestCase):
@@ -9,6 +11,11 @@ class HeroTest(unittest.TestCase):
                          title="Dragonslayer",
                          health=100, mana=100,
                          mana_regeneration_rate=2)
+        self.weapon = Weapon(name="The Axe of Destiny", damage=20)
+        self.spell = Spell(name="Fireball",
+                           damage=30,
+                           mana_cost=50,
+                           cast_range=2)
 
     def test_exists_class(self):
         self.assertTrue(isinstance(self.hero, Hero))
@@ -52,6 +59,26 @@ class HeroTest(unittest.TestCase):
         self.hero.take_damage(damage_points)
         self.hero.reduce_mana(mana_points)
         self.assertTrue(self.hero.take_mana(new_mana_points), 80)
+
+    def test_hero_weapon(self):
+        weapon_equipment = []
+        weapon_equipment.append(self.weapon)
+        self.assertEqual(weapon_equipment[0], self.weapon)
+
+    def test_hero_can_equip_with_weapon(self):
+        self.hero.equip(self.weapon)
+        self.assertEqual(str(self.hero.weapon_equipment[0]),
+                         self.weapon.get_name())
+
+    def test_hero_spell(self):
+        learned_spell = []
+        learned_spell.append(self.spell)
+        self.assertEqual(learned_spell[0], self.spell)
+
+    def test_hero_can_learn_new_spell(self):
+        self.hero.learn(self.spell)
+        self.assertEqual(str(self.hero.learned_spell[0]),
+                         self.spell.get_name())
 
 if __name__ == '__main__':
     unittest.main()
