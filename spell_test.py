f9939ec650489f69bff630821e59fc81261002ab
Rositsazz <ross_zz@mail.bg>
2015-04-20 12:13:50 +0300

    new changes

--- spell_test.py
+++ spell_test.py
@@ -4,7 +4,10 @@ from spell import Spell
 
 class SpellTest(unittest.TestCase):
     def setUp(self):
-        self.my_spell = Spell(name="Fireball", damage=30, mana_cost=50, cast_range=2)
+        self.my_spell = Spell(name="Fireball",
+                              damage=30,
+                              mana_cost=50,
+                              cast_range=2)
 
     def test_create_spell(self):
         self.assertTrue(isinstance(self.my_spell, Spell))
